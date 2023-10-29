3736481cb5f2ca2544b1084c69e90dc4f4fbace7
ValeDg <diazguardamino07@gmail.com>
2023-10-28 21:57:27 -0500

    ...

---
+++ src/main/java/pe/edu/cibertec/apirestcl2DiazGuardamino/ApiRestCl2DiazGuardaminoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.apirestcl2DiazGuardamino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCl2DiazGuardaminoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCl2DiazGuardaminoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/apirestcl2DiazGuardamino/ApiRestCl2DiazGuardaminoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.apirestcl2DiazGuardamino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCl2DiazGuardaminoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
